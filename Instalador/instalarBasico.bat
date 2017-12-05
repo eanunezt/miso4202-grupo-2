bbb4ea214b2b5c708b49e505799e7adcbf949b1f
JairoC <collante.jairo@gmail.com>
2017-12-04 21:54:46 -0500

    Ejecuta el POM principal y undeploy y agrega tiempo de espera entre comandos

--- Instalador/instalarBasico.bat
+++ Instalador/instalarBasico.bat
@@ -1,17 +1,21 @@
-set WORKSPACE=D:\jairo\workspace\MISO\GitHub\FabricaSoftware\grupo-2\marketplace
+set WORKSPACE=D:\jairo\workspace\MISO\GitHub\grupo-2\marketplace
 set WILDFLY=D:\jairo\programas\wildfly-11.0.0.CR1
 
 copy /Y "./project.properties" %WORKSPACE%\marketplace-aspectos\src\main\resources\project.properties
 copy /Y "./project.properties" %WORKSPACE%\marketplace-autenticacion\src\main\resources\project.properties
+copy /Y "./project.properties" %WORKSPACE%\marketplace-compras\src\main\resources\project.properties
 copy /Y "./project.xml" %WORKSPACE%\marketplace-aspectos\src\main\resources\project.xml
 copy /Y "./project.xml" %WORKSPACE%\marketplace-autenticacion\src\main\resources\project.xml
+copy /Y "./project.xml" %WORKSPACE%\marketplace-compras\src\main\resources\project.xml
 
-call mvn clean -f   %WORKSPACE%\marketplace-producto-basico\pom.xml
-call mvn install -f %WORKSPACE%\marketplace-producto-basico\pom.xml
-call mvn clean -f   %WORKSPACE%\marketplace-autenticacion\pom.xml
-call mvn install -f %WORKSPACE%\marketplace-autenticacion\pom.xml
 
-copy /Y %WORKSPACE%\marketplace-basico\target\producto-basico.war %WILDFLY%\standalone\deployments\producto-basico.war
-copy /Y %WORKSPACE%\marketplace-producto-autenticacion\target\marketplace-autenticacion.war %WILDFLY%\standalone\deployments\marketplace-autenticacion.war
+call mvn clean -f   %WORKSPACE%\pom.xml
+call mvn install -f %WORKSPACE%\pom.xml
+
+timeout 5 > NUL
+
+del /Q %WILDFLY%\standalone\deployments\producto-compras.war
+copy /Y %WORKSPACE%\marketplace-producto-basico\target\producto-basico.war %WILDFLY%\standalone\deployments\producto-basico.war
+copy /Y %WORKSPACE%\marketplace-autenticacion\target\marketplace-autenticacion.war %WILDFLY%\standalone\deployments\marketplace-autenticacion.war
 
 start cmd.exe /k %WILDFLY%\bin\standalone.bat
--- Instalador/instalarCompras.bat
+++ Instalador/instalarCompras.bat
@@ -1,17 +1,21 @@
-set WORKSPACE=D:\jairo\workspace\MISO\GitHub\FabricaSoftware\grupo-2\marketplace
+set WORKSPACE=D:\jairo\workspace\MISO\GitHub\grupo-2\marketplace
 set WILDFLY=D:\jairo\programas\wildfly-11.0.0.CR1
 
 copy /Y "./project.properties" %WORKSPACE%\marketplace-aspectos\src\main\resources\project.properties
 copy /Y "./project.properties" %WORKSPACE%\marketplace-autenticacion\src\main\resources\project.properties
+copy /Y "./project.properties" %WORKSPACE%\marketplace-compras\src\main\resources\project.properties
+
 copy /Y "./project.xml" %WORKSPACE%\marketplace-aspectos\src\main\resources\project.xml
 copy /Y "./project.xml" %WORKSPACE%\marketplace-autenticacion\src\main\resources\project.xml
+copy /Y "./project.xml" %WORKSPACE%\marketplace-compras\src\main\resources\project.xml
 
-call mvn clean -f %WORKSPACE%\marketplace-producto-compras\pom.xml
-call mvn install -f %WORKSPACE%\marketplace-producto-compras\pom.xml
-call mvn clean -f %WORKSPACE%\marketplace-autenticacion\pom.xml
-call mvn install -f %WORKSPACE%\marketplace-autenticacion\pom.xml
+call mvn clean -f   %WORKSPACE%\pom.xml
+call mvn install -f %WORKSPACE%\pom.xml
 
-copy /Y %WORKSPACE%\marketplace-compras\target\producto-compras.war %WILDFLY%\standalone\deployments\producto-compras.war
+timeout 5 > NUL
+
+del /Q %WILDFLY%\standalone\deployments\producto-basico.war
+copy /Y %WORKSPACE%\marketplace-producto-compras\target\producto-compras.war %WILDFLY%\standalone\deployments\producto-compras.war
 copy /Y %WORKSPACE%\marketplace-autenticacion\target\marketplace-autenticacion.war %WILDFLY%\standalone\deployments\marketplace-autenticacion.war
 
 start cmd.exe /k %WILDFLY%\bin\standalone.bat
